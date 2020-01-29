f4ae7cf1f6ec61b0ace8e7c39c0209d30697d039
Christopher Floyd <cfloyd252@gmail.com>
2020-01-29 10:07:53 -0700

    complete state drill 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import App from './App';
+// import './index.css';
+import HelloWorld from './state-drills/HelloWorld';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    state = {
+        who: 'world'
+    };
+
+
+
+    render() {
+        return (
+            <div>
+                <p>Hello, {this.state.who}</p>
+                <button onClick={() => this.setState({
+                    who: 'friend'
+                })}>Friend</button>
+                <button onClick={() => this.setState({
+                    who: 'react'
+                })}>React</button>
+                <button onClick={() => this.setState({
+                    who: 'world'
+                })}>World</button>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
